2c6e06a37373e5d8e6253ff61cea4de590f557d3
Cristian Jesús Leon Velásquez <cjleonv@ufpso.edu.co>
2025-06-01 08:48:26 -0500

    Create close app button

--- app/src/main/java/com/logininstagramcompose/MainActivity.kt
+++ app/src/main/java/com/logininstagramcompose/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,6 +19,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             LoginInstagramComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    LoginScreen(Modifier.padding(innerPadding))
 
                 }
             }
