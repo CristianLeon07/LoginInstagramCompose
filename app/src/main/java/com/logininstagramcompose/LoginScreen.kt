bdaed1aebf0d9207aa3350715c5c8ccba4df8380
Cristian Jesús Leon Velásquez <cjleonv@ufpso.edu.co>
2025-06-01 09:09:56 -0500

    Create header login first part

--- app/src/main/java/com/logininstagramcompose/LoginScreen.kt
+++ app/src/main/java/com/logininstagramcompose/LoginScreen.kt
@@ -3,17 +3,28 @@ package com.logininstagramcompose
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -24,6 +35,7 @@ fun LoginScreen(modifier: Modifier) {
             .padding(8.dp)
     ) {
         Header(Modifier.align(Alignment.TopEnd))
+        Body(modifier.align(Alignment.Center))
     }
 
 
@@ -41,5 +53,27 @@ fun Header(modifier: Modifier) {
             activity.finish()
 
         })
+}
+
+@Composable
+fun Body(modifier: Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        ImageLogo()
+        Spacer(modifier.size(16.dp))
+        Email(email) { email = it }
+    }
+}
+
+@Composable
+fun ImageLogo() {
+    Image(painter = painterResource(id = R.drawable.instagram), contentDescription = "logo")
+
+}
+
+@Composable
+fun Email(email: String, onTextChange: (String) -> Unit) {
+    TextField(value = email, onValueChange = { onTextChange(it) })
 }
 
